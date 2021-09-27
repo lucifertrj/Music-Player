f345f918953c9ab859fb347d417e91a293fbc84e
lucifertrj <66197713+lucifertrj@users.noreply.github.com>
2021-09-27 11:50:17 +0530

    🎼Added comments

--- anime_playlist.py
+++ anime_playlist.py
@@ -11,6 +11,8 @@ class AnimePlayer:
         self.track = tk.StringVar()
 
     def playsong(self):
+        #change mp3 to ogg
+        #get the active song from the ListBox
         self.track.set(self.animelist.get("active"))
         self.status.set(" |> Playing...")
         AudioSegment.from_mp3(self.animelist.get("active")).export("play.ogg", format='ogg')
@@ -41,6 +43,7 @@ class AnimePlayer:
         stop_img = tk.PhotoImage(file="stop.png")
         unpause_img = tk.PhotoImage(file="unpause.png")
 
+        #use playsong, pausesong as the trigger functions for the active songs
         play_button = tk.Button(box,image=play_img,command=self.playsong,bd=5,bg="blue")
         play_button.place(x=110,y=375,width=80,height=75)
         pause_button = tk.Button(box,image=pause_img,command=self.pausesong,bd=5,bg="blue")
@@ -51,9 +54,11 @@ class AnimePlayer:
         unpause_button.place(x=310,y=375,width=80,height=75)
         #controls config ends here
 
+        #the main frame where all the song will be displayer
         anime_frame = tk.LabelFrame(self._root,text="Anime Playlist",font=("arial",15,"bold"),bg="black",fg="blue",bd=5)
         anime_frame.place(x=0,y=20,width=400,height=250)
 
+        #this is used to display the status while song is active, active: when user clicks on any song
         statusframe = tk.LabelFrame(self._root,text="Now Playing",font=("arial",15,"bold"),bg="black",fg="blue",bd=5)
         statusframe.place(x=0,y=270,width=400,height=95)
 
@@ -63,20 +68,24 @@ class AnimePlayer:
         status = tk.Label(statusframe,textvariable=self.status,font=("arial", 15,"bold"),bg="black",fg="blue")
         status.pack()
 
+        #configure the scroll-bar which becomes easy to choose a song
         scroll_bar = tk.Scrollbar(anime_frame,orient="vertical")
-        self.animelist = tk.Listbox(anime_frame,yscrollcommand=scroll_bar.set,selectbackground="blue",font=("arial",12,"bold"),bg="grey",fg="navyblue",bd=5)
+        self.animelist = tk.Listbox(anime_frame,yscrollcommand=scroll_bar.set,selectbackground="white",font=("arial",12,"bold"),bg="grey",fg="navyblue",bd=5)
         scroll_bar.pack(side="right",fill="y")
         scroll_bar.config(command=self.animelist.yview)
         self.animelist.pack(fill="both")
 
+        #change the directory where all the openings are saved
         os.chdir("AnimeTrack")
-        anime_songs = os.listdir()
+        anime_songs = os.listdir() #store all songs in a list
         anime_songs.sort()
         for songs in anime_songs[:-1]:
+            #add all songs in the ListBox
             self.animelist.insert(tk.END,songs)
         self._root.mainloop()
 
 if __name__ == '__main__':
+    #create a root window and initialize pygame mixer
     root = tk.Tk()
     root.title("Anime Player")
     pygame.init()
